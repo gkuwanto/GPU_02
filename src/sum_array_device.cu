f7dbf214d525da0f6f89a391a2863cd5377f2e81
Garry Kuwanto <gkuwanto@gmail.com>
2021-02-23 19:47:40 +0700

    add sequential

--- src/sum_array_device.cu
+++ src/sum_array_device.cu
@@ -14,6 +14,28 @@ void naiveSumArray(const float *input, float *output, int n) {
     atomicAdd(output, partial_sum);
 }
 
+__global__
+void sequentialSumArray(const float *input, float *output, int n) {
+    int idx = blockDim.x * blockIdx.x + threadIdx.x;
+    __shared__ float sdata[1024];
+    if (idx < n){
+
+        /*copy to shared memory*/
+        sdata[threadIdx.x] = input[idx];
+        __syncthreads();
+
+        for(int stride=blockDim.x/2; stride > 0; stride /= 2) {
+            if (threadIdx.x < stride) {
+                float lhs = sdata[threadIdx.x];
+                float rhs = sdata[threadIdx.x + stride];
+                sdata[threadIdx.x] = lhs+rhs;
+            }
+            __syncthreads();
+        }
+    }
+    if (idx == 0) data[0] = sdata[0];
+}
+
 
 void cudaSumArray(
     const float *d_input,
@@ -24,6 +46,11 @@ void cudaSumArray(
     if (type == NAIVE) {
         dim3 blockSize(1024, 1);
         dim3 gridSize(n / 1024, 1);
+        naiveSumArray<<<gridSize, blockSize>>>(d_input, d_output, n);
+    }
+    if (type == SEQUENTIAL) {
+        dim3 blockSize(1024, 1);
+        dim3 gridSize(n / 1024 + 1, 1);
         naiveSumArray<<<gridSize, blockSize>>>(d_input, d_output, n);
     }
 }
--- src/sum_array_host.cpp
+++ src/sum_array_host.cpp
@@ -135,6 +135,20 @@ int main(int argc, char *argv[]) {
 
             printf("Size %d naive GPU: %f ms\n", n, naive_gpu_ms);
         }
+
+        if (kernel == "sequential" || kernel == "all") {
+            START_TIMER();
+            cudaSumArray(d_input, d_output, n, SEQUENTIAL);
+            STOP_RECORD_TIMER(naive_gpu_ms);
+
+            gpuErrChk(cudaMemcpy(&output, d_output, sizeof(float), 
+                cudaMemcpyDeviceToHost));
+            checkSumArray(input, output, n);
+            output = 0;
+            gpuErrChk(cudaMemset(d_output, 0, sizeof(float)));
+
+            printf("Size %d sequential GPU: %f ms\n", n, naive_gpu_ms);
+        }
     }
     
     
