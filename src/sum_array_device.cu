4738380a82f5a6bc619a650d4a2b526dcc921aa0
Garry Kuwanto <gkuwanto@gmail.com>
2021-02-23 19:26:32 +0700

    super naive

--- src/sum_array_device.cu
+++ src/sum_array_device.cu
@@ -4,12 +4,7 @@
 
 __global__
 void naiveSumArray(const float *input, float *output, int n) {
-    double sum = 0.0;
-    //reduce multiple elements per thread
-    for (int i=1;i<32;i++)
-        int index = blockIdx.x * blockDim.x + threadIdx.x + gridDim.x * blockDim.x * i;
-        if (index < n)
-            sum += input[index];
+    double sum = input[blockIdx.x * blockDim.x + threadIdx.x];
     atomicAdd(output, sum);
 }
 
@@ -22,7 +17,7 @@ void cudaSumArray(
 {
     if (type == NAIVE) {
         dim3 blockSize(1024, 1);
-        dim3 gridSize(n / 32 / 1024, 1);
+        dim3 gridSize(n / 1024, 1);
         naiveSumArray<<<gridSize, blockSize>>>(d_input, d_output, n);
     }
 }
