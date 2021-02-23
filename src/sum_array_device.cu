ba18945ab2986015c255c5b3a7af810c8e078122
Garry Kuwanto <gkuwanto@gmail.com>
2021-02-23 20:13:55 +0700

    remove sequential

--- src/sum_array_device.cu
+++ src/sum_array_device.cu
@@ -13,29 +13,6 @@ void naiveSumArray(const float *input, float *output, int n) {
     atomicAdd(output, partial_sum);
 }
 
-__global__
-void sequentialSumArray(const float *input, float *output, int n) {
-    int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    __shared__ float sdata[1024];
-    if (idx < n){
-
-        /*copy to shared memory*/
-        sdata[threadIdx.x] = input[idx];
-        __syncthreads();
-
-        for(int stride=blockDim.x/2; stride > 0; stride /= 2) {
-            if (threadIdx.x < stride) {
-                float lhs = sdata[threadIdx.x];
-                float rhs = sdata[threadIdx.x + stride];
-                sdata[threadIdx.x] = lhs+rhs;
-            }
-            __syncthreads();
-        }
-    }
-    if (idx == 0) atomicAdd(output, sdata[0]);
-}
-
-
 void cudaSumArray(
     const float *d_input,
     float *d_output,
@@ -45,11 +22,6 @@ void cudaSumArray(
     if (type == NAIVE) {
         dim3 blockSize(1024, 1);
         dim3 gridSize(1, 1);
-        naiveSumArray<<<gridSize, blockSize>>>(d_input, d_output, n);
-    }
-    if (type == SEQUENTIAL) {
-        dim3 blockSize(1024, 1);
-        dim3 gridSize(n / 1024 + 1, 1);
         naiveSumArray<<<gridSize, blockSize>>>(d_input, d_output, n);
     }
 }
